80d253483ac87580eac41a992493d533b18620cf
gitty-killer <gittykiller@outlook.com>
2026-01-18 02:08:18 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Hello from rndproj-9n4d7ok2");
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# rndproj-9n4d7ok2
+
+Random csharp project for random.
+
